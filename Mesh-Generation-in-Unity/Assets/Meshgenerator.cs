48fd703a8f466aa2f5b2450a87a15c482dce2176
Felipe Bock Magagnin <felipe.bock@hotmail.com>
2018-11-21 15:03:07 -0200

    1.0 - Simple generation of a mesh with some noise

--- Mesh-Generation-in-Unity/Assets/Meshgenerator.cs
+++ Mesh-Generation-in-Unity/Assets/Meshgenerator.cs
@@ -11,13 +11,20 @@ public class Meshgenerator : MonoBehaviour {
 	Vector3[] vertices;
 	int[] triangles;
 
+	public int xSize = 100;
+	public int zSize = 100;
+
 
 	// Use this for initialization
 	void Start () {
 		mesh = new Mesh();
 		GetComponent<MeshFilter>().mesh = mesh;	
 
-		CreateShape();
+		StartCoroutine(CreateShape());
+		
+	}
+
+	void Update(){
 		UpdateMesh();
 	}
 
@@ -30,17 +37,44 @@ public class Meshgenerator : MonoBehaviour {
 		mesh.RecalculateNormals();
 	}
 	
-	void CreateShape(){
-		vertices = new Vector3[]{
-			new Vector3(0,0,0),
-			new Vector3(0,0,1),
-			new Vector3(1,0,0),
-			new Vector3(1,0,1)
-		};
+	IEnumerator CreateShape(){
+		vertices = new Vector3[(xSize + 1) * (zSize + 1)];		
 
-		triangles = new int[]{
-			0,1,2,
-			1,3,2
-		};
+		for (int i = 0, z = 0; z <= zSize; z++){
+			for (int x = 0; x <= xSize; x++){
+				float y = Mathf.PerlinNoise(x * .3f,z * .3f) * 3f;
+				vertices[i] = new Vector3(x,y, z);
+				i++;
+			}
+		}
+
+
+
+		int vert =0;
+		int tris = 0;
+		triangles = new int[xSize * zSize * 6];
+
+		for (int z =0; z < xSize; z++){
+			for (int x = 0; x < xSize; x++){
+			
+			triangles[0 + tris] = vert + 0;
+			triangles[1 + tris] = vert + xSize + 1;
+			triangles[2 + tris] = vert + 1;
+			triangles[3 + tris] = vert + 1;
+			triangles[4 + tris] = vert + xSize + 1;
+			triangles[5 + tris] = vert + xSize + 2;
+
+			vert++;
+			tris += 6;
+
+			yield return new WaitForSeconds(.001f);
+			}
+		vert++;
+		}
+
+		
+
+		
+
 	}
 }
